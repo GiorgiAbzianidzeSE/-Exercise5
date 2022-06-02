4dc7f4a7038472109dfddc20a6a708403b61472d
GiorgiAbzianidzeSE <84072183+GiorgiAbzianidzeSE@users.noreply.github.com>
2022-06-02 22:52:19 +0400

    Add files via upload

---
+++ tbc/course/tasks/Square.java
@@ -0,0 +1,17 @@
+package tbc.course.tasks;
+
+import ge.ufc.exercises.Rectangle;
+
+public final class Square extends Rectangle {
+    private float side;
+
+    @Override
+    protected double perimeterCalculator(){
+        return super.perimeterCalculator();
+    }
+
+    @Override
+    protected double areaCalculator(){
+        return super.areaCalculator();
+    }
+}
